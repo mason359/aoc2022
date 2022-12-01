d56f4b36b599ba26cd68412ce2216305a6641fc9
Mason Murphy <mrmurphy888@gmail.com>
2022-12-01 00:12:15 -0800

    Solve day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,13 +1,33 @@
 use crate::Day;
 
+use itertools::Itertools;
+
 pub struct Day1;
 
 impl Day for Day1 {
-    fn part1(&self, _input: String) -> i64 {
-        1
+    fn part1(&self, input: String) -> i64 {
+        input
+            .split("\n\n")
+            .map(|elf| elf
+                .lines()
+                .map(|food| food.parse::<i64>().unwrap())
+                .sum::<i64>()
+            )
+            .max()
+            .unwrap()
     }
 
-    fn part2(&self, _input: String) -> i64 {
-        2
+    fn part2(&self, input: String) -> i64 {
+        input
+            .split("\n\n")
+            .map(|elf| elf
+                .lines()
+                .map(|food| food.parse::<i64>().unwrap())
+                .sum::<i64>()
+            )
+            .sorted()
+            .rev()
+            .take(3)
+            .sum()
     }
 }
